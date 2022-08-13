154ba38f263bb0f5bddd5dfc9315e832d02fff43
Joe Forrester <joe.forrester@choreograph.com>
2022-08-12 22:17:21 -0500

    Variables lesson completed.  Additional code added.

--- firstSteps/HelloWorld/src/Hello.java
+++ firstSteps/HelloWorld/src/Hello.java
@@ -2,6 +2,10 @@ public class Hello {
 
     public static void main(String[] args) {
         System.out.println("Hello, Joe");
+
+        int myFirstNumber = (10 + 5) + (2 * 10);
+
+        System.out.println("myFirstNumber: " + myFirstNumber);
     }
 
 }
